ea4ecce9798fcfcbdd59b92f89d1794eeb4d3ec3
Denys <denisbadakaty51@gmail.com>
2024-01-29 16:35:46 +0200

    add eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,208 @@
+const initialRules = {
+  'no-magic-numbers': 'error',
+  'consistent-return': 'off',
+  'eslint-comments/disable-enable-pair': 'off',
+  'eslint-comments/require-description': [
+    'warn',
+    { ignore: ['eslint-enable'] },
+  ], // we don't need to comment why we used "eslint-enable"
+  'quote-props': ['error', 'consistent-as-needed'],
+  quotes: ['error', 'single', { avoidEscape: true }],
+  'arrow-parens': ['error', 'always'],
+  curly: ['error', 'all'],
+  'prefer-destructuring': 'error',
+  'default-case': 'error',
+  'no-restricted-exports': [
+    'error',
+    { restrictDefaultExports: { defaultFrom: true } },
+  ],
+  'func-names': ['error', 'always', { generators: 'never' }],
+  'typescript-sort-keys/interface': [
+    'error',
+    'asc',
+    { caseSensitive: false, natural: false, requiredFirst: true },
+  ],
+  'no-void': ['error', { allowAsStatement: true }], // we allow to use "void" to mark promises we don't wait for
+  'no-unused-expressions': ['error'], // we prefer to use callFunction?.() instead of callFunction && callFunction()
+  'no-empty-function': [
+    'error',
+    {
+      allow: ['constructors'],
+    },
+  ],
+  'no-dupe-keys': 'error',
+  'no-console': ['error', { allow: ['warn', 'error', 'debug'] }],
+  'no-underscore-dangle': ['off'], // we regulate an use of an underscore by other rules
+  'no-plusplus': 'off', // It's okay to use ++ operator
+  'no-magic-numbers': [
+    'error',
+    {
+      ignore: [-1, 0, 1],
+      ignoreArrayIndexes: true,
+      ignoreDefaultValues: true,
+      ignoreClassFieldInitialValues: true,
+    },
+  ],
+  'no-multiple-empty-lines': ['error', { max: 2, maxBOF: 0, maxEOF: 1 }],
+};
+
+const tsRules = {
+  '@typescript-eslint/no-shadow': 'error', // Vars with the same name in different scopes are not allowed
+  '@typescript-eslint/no-unused-vars': [
+    'error',
+    { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' },
+  ], // Ignore variables with "_" prefix
+  '@typescript-eslint/no-unused-expressions': ['error'],
+  'quote-props': 'off',
+  '@typescript-eslint/explicit-function-return-type': [
+    'error',
+    {
+      allowExpressions: true,
+      allowFunctionsWithoutTypeParameters: true,
+    },
+  ],
+  '@typescript-eslint/no-floating-promises': 'error',
+  '@typescript-eslint/unbound-method': 'off',
+  '@typescript-eslint/no-use-before-define': [
+    'error',
+    {
+      variables: false, // we should disable this since we use all "styles" vars before their definition
+    },
+  ],
+  '@typescript-eslint/no-inferrable-types': 'off', // we should always set types, even if they are trivial (number, boolean, etc)
+  '@typescript-eslint/no-unsafe-assignment': 'error',
+  '@typescript-eslint/no-unsafe-member-access': 'error',
+  '@typescript-eslint/no-var-requires': 'error',
+  '@typescript-eslint/no-explicit-any': 'error',
+  '@typescript-eslint/require-await': 'error',
+  '@typescript-eslint/no-unsafe-argument': 'error',
+  '@typescript-eslint/no-unsafe-call': 'error',
+  '@typescript-eslint/no-redundant-type-constituents': 'error',
+  '@typescript-eslint/no-unsafe-return': 'error',
+  '@typescript-eslint/no-misused-promises': [
+    'error',
+    {
+      checksVoidReturn: false,
+    },
+  ],
+};
+
+const reactRules = {
+  'react/react-in-jsx-scope': 'off',
+  'react/jsx-uses-react': 'off',
+  'react/display-name': 'off',
+  'react/prop-types': 'off',
+  'react/no-array-index-key': 'off',
+  'react-hooks/exhaustive-deps': 'off',
+  'react/style-prop-object': 'off', // we allow to use string as prop
+  'react/require-default-props': 'off',
+  'react/jsx-props-no-spreading': 'off',
+  'react/no-unescaped-entities': 'off',
+  'react/function-component-definition': [
+    'error',
+    {
+      namedComponents: 'arrow-function',
+      unnamedComponents: 'arrow-function',
+    },
+  ],
+};
+
+const importSortOrderRule = {
+  'simple-import-sort/imports': [
+    'error',
+    {
+      groups: [
+        ['^(@?\\w|\\w)'], // libs
+        ['^$\\.*'], // ts-aliases
+        ['^\\.\\./'], // parent folder imports
+        ['^\\./'], // relative folder imports
+      ],
+    },
+  ],
+};
+
+const importRules = {
+  'no-duplicate-imports': 'error', // imports from the same source must be in one record
+  'import/no-cycle': ['error', { maxDepth: '∞' }],
+  'import/prefer-default-export': 'off', // we use only named exports in the project
+  'import/extensions': 'off',
+  'import/no-extraneous-dependencies': ['error'],
+  ...importSortOrderRule,
+};
+
+const paddingRules = {
+  'padding-line-between-statements': [
+    'error',
+    {
+      blankLine: 'always',
+      prev: '*',
+      next: [
+        'return',
+        'if',
+        'export',
+        'function',
+        'while',
+        'try',
+        'throw',
+        'class',
+      ],
+    },
+    {
+      blankLine: 'always',
+      prev: ['if', 'function', 'while', 'export', 'throw', 'class'],
+      next: '*',
+    },
+    { blankLine: 'any', prev: 'const', next: ['const', 'let'] },
+    { blankLine: 'always', prev: 'const', next: '*' },
+    { blankLine: 'any', prev: 'const', next: 'const' },
+    {
+      blankLine: 'always',
+      prev: 'multiline-const',
+      next: '*',
+    },
+    {
+      blankLine: 'always',
+      prev: '*',
+      next: 'multiline-const',
+    },
+  ],
+};
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'next/core-web-vitals',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:eslint-comments/recommended',
+    'plugin:@cspell/recommended',
+  ],
+  parserOptions: {
+    project: 'tsconfig.json',
+    ecmaVersion: 2023,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'eslint-plugin-import',
+    'simple-import-sort',
+    'typescript-sort-keys',
+    'react',
+  ],
+  ignorePatterns: ['.eslintrc.js', '.prettierrc.js', 'next.config.mjs'],
+  rules: {
+    ...initialRules,
+    ...tsRules,
+    ...reactRules,
+    ...importRules,
+    ...paddingRules,
+  },
+};
