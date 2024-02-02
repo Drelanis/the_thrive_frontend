ac4988fb5825f2a2ce57264fc128ac16b0338983
Denys <denisbadakaty51@gmail.com>
2024-02-02 13:29:37 +0200

    add adpater and common types

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,9 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import { DefaultJWT } from '@configs';
 import { db } from '@lib/db';
-import NextAuth from 'next-auth';
+import { EmployeeRoles } from '@server/types';
+import { getCompanyById } from '@server/utils';
+import NextAuth, { Session } from 'next-auth';
 
 import authConfig from './auth.config';
 
@@ -13,6 +16,42 @@ export const {
   adapter: PrismaAdapter(db),
   session: {
     strategy: 'jwt',
+  },
+  callbacks: {
+    // async signIn({ user }) {
+    //   const existingUser = await getCompanyById(user.id);
+
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false;
+    //   }
+
+    //   return true;
+    // },
+    session({ session, token }: { session: Session; token?: DefaultJWT }) {
+      const { user } = session;
+
+      if (token) {
+        const { sub: id, role } = token;
+
+        user.id = id;
+        user.role = role as EmployeeRoles;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      const { sub } = token;
+
+      const existingUser = await getCompanyById(sub);
+
+      if (!existingUser) {
+        return token;
+      }
+
+      token.role = existingUser.role;
+
+      return token;
+    },
   },
   ...authConfig,
 });
