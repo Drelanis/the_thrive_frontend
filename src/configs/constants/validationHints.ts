0807c0b358c1af3294255222e1b831a3f18b046d
Denys <denisbadakaty51@gmail.com>
2024-02-07 12:39:17 +0200

    add validation hints

---
+++ src/configs/constants/validationHints.ts
@@ -0,0 +1,6 @@
+export enum ValidationHints {
+  REQUIRED = 'This field is required',
+  INVALID_EMAIL = 'This field must be valid email',
+  PASSWORDS_MISMATCH = "Passwords don't match",
+  PASSWORD_SHORT = 'Password is too short, at least six characters',
+}
