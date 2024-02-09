812bc7f5a16f19b81bd727541fccedc79bc612c1
Denys <denisbadakaty51@gmail.com>
2024-02-09 12:53:10 +0200

    minor fix

--- src/server/actions/auth/signIn/index.ts
+++ src/server/actions/auth/signIn/index.ts
@@ -1,16 +1,19 @@
 'use server';
 
-import { Routes, SigninDto } from '@configs';
+import { Providers, SigninDto } from '@configs';
 import { signInValidationSchema } from '@modules/stores';
 import { NASignIn } from '@server/nextAuth';
 import { SignInErrorResponse } from '@server/utils';
+
+import { checkEmailVerification } from '../verificationToken';
 
 export const signIn = async (values: SigninDto) => {
   try {
     await signInValidationSchema.validate(values);
 
-    await NASignIn('credentials', {
-      redirectTo: Routes.DASHBOARD,
+    await checkEmailVerification(values.email);
+
+    await NASignIn(Providers.CREDENTIALS, {
       ...values,
     });
   } catch (error: unknown) {
