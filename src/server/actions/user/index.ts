6c15164677568ba2e9f8ddf41f7072b6391b64d9
Denys <denisbadakaty51@gmail.com>
2024-02-09 12:51:20 +0200

    update user actions

--- src/server/actions/user/index.ts
+++ src/server/actions/user/index.ts
@@ -3,54 +3,82 @@ import { db } from '@lib';
 import * as bcrypt from 'bcryptjs';
 
 export const getUserById = async (id: string): Promise<UserType | null> => {
-  const existingUser = await db.user.findUnique({
-    where: {
-      id,
-    },
-  });
+  try {
+    const existingUser = await db.user.findUnique({
+      where: {
+        id,
+      },
+    });
 
-  return existingUser;
+    return existingUser;
+  } catch (error) {
+    return null;
+  }
 };
 
 export const getUserByEmail = async (
   email: string,
 ): Promise<UserType | null> => {
-  const existingUser = await db.user.findUnique({
-    where: {
-      email,
-    },
-  });
+  try {
+    const existingUser = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
 
-  return existingUser;
+    return existingUser;
+  } catch (error) {
+    return null;
+  }
 };
 
 export const createUser = async (dto: Omit<SignUpDto, 'repeatPassword'>) => {
-  const { firstName, lastName, email, password } = dto;
+  try {
+    const { firstName, lastName, email, password } = dto;
 
-  const name = `${firstName} ${lastName}`;
+    const name = `${firstName} ${lastName}`;
 
-  const existingCompany = await getUserByEmail(email);
+    const existingCompany = await getUserByEmail(email);
 
-  if (existingCompany) {
-    throw new Error(
-      'A company with this email address has already been created',
+    if (existingCompany) {
+      throw new Error(
+        'A company with this email address has already been created',
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(
+      password,
+      Number(process.env.BCRYPT_SALT) || 0,
     );
+
+    const user = await db.user.create({
+      data: {
+        name,
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return user;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const updateUserByEmailVerified = async (email: string) => {
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser) {
+    throw new Error('Email does not exist!');
   }
 
-  const hashedPassword = await bcrypt.hash(
-    password,
-    Number(process.env.BCRYPT_SALT) || 0,
-  );
-
-  const user = await db.user.create({
+  await db.user.update({
+    where: { id: existingUser.id },
     data: {
-      name,
-      firstName,
-      lastName,
+      emailVerified: new Date(),
       email,
-      password: hashedPassword,
     },
   });
-
-  return user;
 };
