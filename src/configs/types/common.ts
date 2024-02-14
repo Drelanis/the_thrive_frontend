7f93312fe267ad83480c54994f447237e1089661
Denys <denisbadakaty51@gmail.com>
2024-02-14 20:23:57 +0200

    update types

--- src/configs/types/common.ts
+++ src/configs/types/common.ts
@@ -7,3 +7,9 @@ export interface DefaultJWT extends Record<string, unknown> {
   picture?: string | null;
   sub?: string;
 }
+
+export type SuccessResponse = {
+  isError: boolean;
+  isTwoFactor?: boolean;
+  message?: string;
+};
