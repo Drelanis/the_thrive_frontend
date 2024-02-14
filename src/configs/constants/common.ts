6b0137bf26df3cc5fd1b1db42c2c3c31a4ffb063
Denys <denisbadakaty51@gmail.com>
2024-02-14 20:11:14 +0200

    update constansts

--- src/configs/constants/common.ts
+++ src/configs/constants/common.ts
@@ -1,5 +1,7 @@
 export const MAX_PASSWORD_VALUE = 6;
-export const TOKEN_EXPIRES = 60;
+export const MAX_TWO_FACTOR_VALUE = 6;
+export const EMAIL_VERIFICATION_TOKEN_EXPIRES = 60;
+export const TWO_FACTOR_TOKEN_EXPIRES = 5;
 
 export enum Providers {
   GOOGLE = 'google',
