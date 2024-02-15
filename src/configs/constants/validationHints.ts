64ab1a7b6c4a8a3b24c0463107e079631a7e4bd5
Denys <denisbadakaty51@gmail.com>
2024-02-15 19:00:09 +0200

    update hints

--- src/configs/constants/validationHints.ts
+++ src/configs/constants/validationHints.ts
@@ -4,4 +4,5 @@ export enum ValidationHints {
   PASSWORDS_MISMATCH = "Passwords don't match",
   PASSWORD_SHORT = 'Password is too short, at least six characters',
   TWO_FACTOR_SHORT = 'Two factor code is too short, at least six characters',
+  EQUALITY_OF_THE_NEW_PASSWORD = 'The new password must not be the same as the old one',
 }
