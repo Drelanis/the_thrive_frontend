860cb9a63f6a61ac095dd7f8d1c89306c1e5560b
Denys <denisbadakaty51@gmail.com>
2024-02-15 14:02:01 +0200

    update helpers

--- src/server/actions/auth/signIn/helpers.ts
+++ src/server/actions/auth/signIn/helpers.ts
@@ -1,62 +1,27 @@
-import { sendTwoFactorTokenEmail } from '@lib';
-import { SuccessResponse } from '@server/utils';
-
-// eslint-disable-next-line import/no-cycle
 import {
-  deleteTwoFactorToken,
-  generateTwoFactorToken,
-  getTwoFactorConfirmationByUserId,
-  getTwoFactorTokenByEmail,
-  updateTwoFactorConfirmation,
-} from '../twoFactorToken';
+  twoFactorCodeVerification,
+  twoFactorDataVerification,
+} from '@server/actions/auth/twoFactorToken';
 
-export const sendTwoFactor = async (email: string) => {
-  const twoFactorToken = await generateTwoFactorToken(email);
-
-  await sendTwoFactorTokenEmail(twoFactorToken.email, twoFactorToken.token);
-
-  return SuccessResponse({
-    message: 'Confirm two-factor authentication!',
-    extraData: { isTwoFactor: true },
-  });
-};
-
-export const twoFactorDataVerification = async (
+export const handleTwoFactorAuth = async (
   userId: string,
-  email: string,
+  isTwoFactorEnabled: boolean,
+  email: string | null,
+  twoFactorCode?: string,
 ) => {
-  const tokenConfirmation = await getTwoFactorConfirmationByUserId(userId);
-
-  if (!tokenConfirmation) {
-    throw new Error('Something went wrong');
+  if (!email) {
+    return;
   }
 
-  let expired = false;
+  if (isTwoFactorEnabled && !twoFactorCode) {
+    const response = await twoFactorDataVerification(userId, email);
 
-  if (tokenConfirmation?.expires) {
-    expired = new Date() > tokenConfirmation?.expires;
+    if (response) {
+      return response;
+    }
   }
 
-  if (expired) {
-    const response = await sendTwoFactor(email);
-
-    return response;
+  if (twoFactorCode) {
+    await twoFactorCodeVerification(email, twoFactorCode);
   }
-};
-
-export const twoFactorCodeVerification = async (
-  email: string,
-  twoFactorCode: string,
-) => {
-  const twoFactorToken = await getTwoFactorTokenByEmail(email);
-
-  const isCodeMatch = twoFactorToken!.token === twoFactorCode;
-
-  if (!isCodeMatch) {
-    throw new Error('Invalid code!');
-  }
-
-  await deleteTwoFactorToken(twoFactorToken!.id);
-
-  await updateTwoFactorConfirmation(email);
 };
