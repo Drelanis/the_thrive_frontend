a9f597e9ab7b8228ab4bab35bb371f73080fbe33
Denys <denisbadakaty51@gmail.com>
2024-02-15 16:30:10 +0200

    add password reset token actions

--- src/server/actions/auth/index.ts
+++ src/server/actions/auth/index.ts
@@ -2,3 +2,5 @@ export * from './signUp';
 export * from './signIn';
 export * from './verificationToken';
 export * from './twoFactorToken';
+export * from './resetPassword';
+export * from './resetPasswordToken';
