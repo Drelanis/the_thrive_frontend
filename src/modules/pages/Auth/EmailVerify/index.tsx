e243dfd13025254515a7ff9b7479be7f01181827
Denys <denisbadakaty51@gmail.com>
2024-02-15 14:17:29 +0200

    minor fix

--- src/modules/pages/Auth/EmailVerify/index.tsx
+++ src/modules/pages/Auth/EmailVerify/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { AuthContainer, AuthTitle } from '@common';
+import { AuthTitle } from '@common';
 
 import { ErrorBody } from './components';
 import { SpinnerStyled, SuccessIcon } from './styles';
@@ -10,13 +10,13 @@ export const EmailVerify = () => {
   const { isPending, isVerified, sendVerificationEmail, isError } = useLogic();
 
   return (
-    <AuthContainer>
+    <>
       <AuthTitle header="Authorization" subHeader="Confirm email" />
       {isVerified && !isPending && <SuccessIcon />}
       {isPending && <SpinnerStyled size="10rem" />}
       {isError && (
         <ErrorBody isPending={isPending} onClick={sendVerificationEmail} />
       )}
-    </AuthContainer>
+    </>
   );
 };
