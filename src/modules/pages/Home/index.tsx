5a52a9dcbf8c61706ca541739fd22a725cbc54b6
Denys <denisbadakaty51@gmail.com>
2024-02-16 11:00:25 +0200

    minor fix

--- src/modules/pages/Home/index.tsx
+++ src/modules/pages/Home/index.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { Routes } from '@configs';
+import Link from 'next/link';
+
 export const Home = () => {
-  return <div>Home</div>;
+  return <Link href={Routes.SING_IN}>AUTH</Link>;
 };
