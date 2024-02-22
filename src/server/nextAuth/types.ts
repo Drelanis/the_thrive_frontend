14f771bd256cb35bce7a1825e21a330fafd5ad6b
Denys <denisbadakaty51@gmail.com>
2024-02-22 18:46:49 +0200

    update types

--- src/server/nextAuth/types.ts
+++ src/server/nextAuth/types.ts
@@ -1,8 +1,8 @@
 import { AdapterUser } from '@auth/core/adapters';
 import { JWT } from '@auth/core/jwt';
-import { Account, Awaitable, Profile, User } from '@auth/core/types';
+import { Account, Awaitable, Profile } from '@auth/core/types';
 import { DefaultJWT } from '@configs';
-import { Session } from 'next-auth';
+import { Session, User } from 'next-auth';
 
 type SessionParams = {
   session: Session;
@@ -10,12 +10,13 @@ type SessionParams = {
 };
 
 type JWTParams = {
-  token: JWT;
+  token: JWT & { agent: string };
+  user: (User | AdapterUser) & { agent: string };
 };
 
 type SignInParams = {
   account: Account | null;
-  user: User | AdapterUser;
+  user: (User | AdapterUser) & { agent: string };
   profile?: Profile;
 };
 
