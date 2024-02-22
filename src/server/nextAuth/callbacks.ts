8bc431ae76b00158a112961526ef6fab57854d16
Denys <denisbadakaty51@gmail.com>
2024-02-22 19:13:16 +0200

    update callbacks

--- src/server/nextAuth/callbacks.ts
+++ src/server/nextAuth/callbacks.ts
@@ -12,109 +12,138 @@ import {
   updateUserImageById,
 } from '@server/actions/user';
 import jwt from 'jsonwebtoken';
+import { userAgent } from 'next/server';
 
 import { CallbacksType } from './types';
 
-export const callbacks: CallbacksType = {
-  async signIn({ user, account, profile }) {
-    const { id } = user;
+export const getCallbacks = (request?: Request): CallbacksType => {
+  return {
+    async signIn({ user, account, profile }) {
+      const { id } = user;
 
-    if (!id) {
-      return false;
-    }
-
-    const existingUser = await getUserById(id);
-
-    if (!existingUser) {
-      return false;
-    }
-
-    if (account?.type !== Providers.CREDENTIALS) {
-      if (!existingUser?.emailVerified) {
-        await updateUserEmailVerifiedById(id);
-      }
-
-      if (!existingUser?.image && profile?.picture) {
-        await updateUserImageById(id, profile?.picture as string);
-      }
-
-      await upsertSession(existingUser);
-
-      return true;
-    }
-
-    if (!existingUser?.emailVerified) {
-      return false;
-    }
-
-    if (existingUser.isTwoFactorEnabled) {
-      const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
-        existingUser.id,
-      );
-
-      if (!twoFactorConfirmation) {
+      if (!id) {
         return false;
       }
-    }
 
-    return true;
-  },
-  async session({ session, token }) {
-    const { user } = session;
+      const existingUser = await getUserById(id);
 
-    if (token && user) {
-      const { sessionId } = token;
-
-      if (!sessionId) {
-        return session;
+      if (!existingUser) {
+        return false;
       }
 
-      let userSession = await getSessionById(sessionId);
-
-      if (!userSession) {
-        return session;
+      if (account?.type === Providers.CREDENTIALS) {
+        await upsertSession(existingUser, user.agent);
       }
 
-      if (new Date(userSession.expires) < new Date()) {
-        const updatedSession = await updateSessionByUserId(userSession.userId);
+      if (account?.type !== Providers.CREDENTIALS) {
+        if (!existingUser?.emailVerified) {
+          await updateUserEmailVerifiedById(id);
+        }
 
-        userSession = updatedSession!;
+        if (!existingUser?.image && profile?.picture) {
+          await updateUserImageById(id, profile?.picture as string);
+        }
+
+        if (request) {
+          const linkedUserAgent = userAgent(request);
+
+          await upsertSession(existingUser, linkedUserAgent.ua);
+        }
+
+        return true;
       }
 
-      const payload = jwt.verify(
-        userSession?.sessionToken,
-        process.env.AUTH_SECRET!,
-      ) as UserType;
+      if (!existingUser?.emailVerified) {
+        return false;
+      }
 
-      user.id = payload.id;
-      user.role = payload.role as UserRoles;
-      user.isTwoFactorEnabled = payload.isTwoFactorEnabled;
-      user.emailVerified = payload.emailVerified;
-      user.image = payload.image;
-      user.name = payload.name;
-      user.firstName = payload.firstName;
-      user.lastName = payload.lastName;
-    }
+      if (existingUser.isTwoFactorEnabled) {
+        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
+          existingUser.id,
+        );
 
-    return session;
-  },
-  async jwt(params) {
-    const { token } = params;
+        if (!twoFactorConfirmation) {
+          return false;
+        }
+      }
 
-    const { sub } = token;
+      return true;
+    },
+    async session({ session, token }) {
+      const { user } = session;
 
-    if (!sub) {
-      return token;
-    }
+      if (token && user) {
+        const { sessionId, agent } = token;
 
-    const existingUser = await getUserById(sub);
+        if (!sessionId) {
+          return session;
+        }
 
-    const session = await getSessionByUserId(sub);
+        let userSession = await getSessionById(sessionId);
 
-    if (!existingUser || !session) {
-      return token;
-    }
+        if (!userSession) {
+          return session;
+        }
 
-    return { sessionId: session.id };
-  },
+        if (new Date(userSession.expires) < new Date()) {
+          const updatedSession = await updateSessionByUserId(
+            userSession.userId,
+            agent,
+          );
+
+          userSession = updatedSession!;
+        }
+
+        const payload = jwt.verify(
+          userSession?.sessionToken,
+          process.env.AUTH_SECRET!,
+        ) as UserType;
+
+        user.id = payload.id;
+        user.role = payload.role as UserRoles;
+        user.isTwoFactorEnabled = payload.isTwoFactorEnabled;
+        user.emailVerified = payload.emailVerified;
+        user.image = payload.image;
+        user.name = payload.name;
+        user.firstName = payload.firstName;
+        user.lastName = payload.lastName;
+        user.agent = agent;
+      }
+
+      return session;
+    },
+    async jwt(params) {
+      const { token, user } = params;
+
+      const { sub } = token;
+
+      let agent = '';
+
+      if (user) {
+        agent = user.agent;
+      }
+
+      if (token.agent) {
+        agent = token.agent;
+      }
+
+      if (request) {
+        agent = userAgent(request).ua;
+      }
+
+      if (!sub) {
+        return token;
+      }
+
+      const existingUser = await getUserById(sub);
+
+      const session = await getSessionByUserId(sub, agent);
+
+      if (!existingUser || !session) {
+        return token;
+      }
+
+      return { sessionId: session.id, agent, sub: existingUser.id };
+    },
+  };
 };
